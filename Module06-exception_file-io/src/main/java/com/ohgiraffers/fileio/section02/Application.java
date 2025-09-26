33ffb8982faf705456350a2989f2c5c271a40167
betterb2etr <b2etr@naver.com>
2025-09-26 15:03:13 +0900

    feat: Module06_IO

---
+++ Module06-exception_file-io/src/main/java/com/ohgiraffers/fileio/section02/Application.java
@@ -0,0 +1,9 @@
+package com.ohgiraffers.fileio.section02;
+
+public class Application {
+    public static void main(String[] args) {
+        try(
+                Buffer
+                )
+    }
+}
